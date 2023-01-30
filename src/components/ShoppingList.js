fa43a7ca331411e49b134431b0de2841a8c71094
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-01-30 18:17:00 -0500

    added filter search state in ShoppingList.js

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,9 +5,14 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState('')
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+  }
+
+  function handleSearchChange(e) {
+    setSearch(e.target.value)
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -19,7 +24,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
